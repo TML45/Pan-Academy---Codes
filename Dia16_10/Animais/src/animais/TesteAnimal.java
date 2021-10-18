90c9dfc31ff43e3792a583ef1afcd10d9010f1dc
TML45 <tassio.melo45@gmail.com>
2021-10-18 18:58:07 -0300

    Update Animal

--- Dia16_10/Animais/src/animais/TesteAnimal.java
+++ Dia16_10/Animais/src/animais/TesteAnimal.java
@@ -7,6 +7,10 @@ public class TesteAnimal {
 		cachorro.setComunicar("Latir");
 		cachorro.setMovimentar("Caminhar");
 		
+		Ave curio = new Ave();
+		curio.setComunicar("cantar");
+		curio.setMovimentar("voar");
+		
 //		System.out.println(p1.toString());
 //		System.out.println(p1.bonusSalario());
 //		
@@ -15,7 +19,10 @@ public class TesteAnimal {
 //		ptg.setSalario(1000);
 //		System.out.println(ptg.bonusSalario());
 		
-		System.out.println(cachorro.getComunicar());	
+		System.out.println(cachorro.getComunicar());
+		System.out.println(cachorro.getMovimentar());
+		System.out.println(curio.getComunicar());
+		System.out.println(curio.getMovimentar());		
 	}
 
 }
