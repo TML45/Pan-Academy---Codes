400539c9d5078020940040bc6fd2fc61952b7ad5
TML45 <tassio.melo45@gmail.com>
2021-10-27 23:04:42 -0300

    Dia27_10_API_REST

---
+++ Dia19_10/EqualsHashCode_Jonathan/src/Collection/Cliente.java
@@ -0,0 +1,21 @@
+package Collection;
+
+
+public class Cliente {
+	
+	public String nome;
+	public int idade;
+	
+	@Override
+	public boolean equals(Object obj) {
+		
+		if(obj instanceof Cliente) {
+			Cliente outro = (Cliente) obj;
+			boolean nomeIgual = outro.nome.equals(this.nome);
+			boolean idadeIgual = outro.idade == this.idade;
+			return nomeIgual && idadeIgual;
+		} else {
+			return false; 
+		}
+	}
+}
---
+++ Dia27_10/api/api/src/main/java/com/example/api/service/UsuarioService.java
@@ -0,0 +1,26 @@
+package com.example.api.service;
+
+import java.util.Optional;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.api.model.Usuario;
+import com.example.api.repositories.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+	@Autowired
+	private UsuarioRepository repository;
+
+	public Usuario findById(Integer id) {
+		Optional<Usuario> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+
+	public List<Usuario> findAll() {
+		return repository.findAll();
+	}
+
+}
