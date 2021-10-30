0765c64239d79ff84d1c3819f461a7e7175010cd
TML45 <tassio.melo45@gmail.com>
2021-10-29 21:08:29 -0300

    Dia29_10

--- Dia27_10/api/api/src/main/java/com/example/api/ApiApplication.java
+++ Dia27_10/api/api/src/main/java/com/example/api/ApiApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.api.model.Produto;
 import com.example.api.model.Usuario;
+import com.example.api.repositories.ProdutoRepository;
 import com.example.api.repositories.UsuarioRepository;
 
 @SpringBootApplication
@@ -15,16 +17,21 @@ public class ApiApplication implements CommandLineRunner {
 
 	@Autowired
 	UsuarioRepository usuarioRepository;
-
+	@Autowired
+	ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
-
+	
 	@Override
 	public void run(String... args) throws Exception {
 		Usuario u1 = new Usuario("Maria Silva", "1234");
 		Usuario u2 = new Usuario("Joao da Silva", "1234");
+		Produto p1 = new Produto("Impressora HP");
+		Produto p2 = new Produto("Monitor Dell");
 		usuarioRepository.saveAll(Arrays.asList(u1, u2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2));
 
 		// usuarioRepository.save(u1);
 		// usuarioRepository.save(u2);
